1f5cc1a74a54deb43dffcedddfb03053d6d8ff79
Tomislav Huđek <tomislav.hudek-fve@bullhorn.com>
2022-08-26 18:34:00 +0200

    encoding plus sign refactor

---
+++ WebApp/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Extensions;
+
+public static class StringExtensions
+{
+    public static string EncodePlusSign(this string str)
+    {
+        return str.Replace("+", "%2B");
+    }
+}
