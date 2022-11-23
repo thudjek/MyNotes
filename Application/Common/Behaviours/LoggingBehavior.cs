f289657ca01057d7e853243108b5eb8a6ad9802d
Tomislav Huđek <thudjek@gmail.com>
2022-11-23 15:17:04 +0100

    .NET 7 migration

--- Application/Common/Behaviours/LoggingBehavior.cs
+++ Application/Common/Behaviours/LoggingBehavior.cs
@@ -12,7 +12,7 @@ public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, 
         _logger = logger;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var requestName = typeof(TRequest).Name;
 
--- Application/Common/Behaviours/UnhandeledExceptionBehavior.cs
+++ Application/Common/Behaviours/UnhandeledExceptionBehavior.cs
@@ -12,7 +12,7 @@ public class UnhandeledExceptionBehavior<TRequest, TResponse> : IPipelineBehavio
         _logger = logger;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var ignoreLoggingExceptions = new List<Type>() { typeof(ValidationException) };
         var requestName = typeof(TRequest).Name;
